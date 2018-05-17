1204343e1fc08e48ee2d90285de8ea21dadb809d
lleohao <lleohao@hotmail.com>
2018-05-17 15:58:14 +0800

    test: add test case

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,62 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-modern-typescript:app', () => {
-  beforeAll(() => {
+describe("generator-modern-typescript:app", () => {
+  const allFile = [
+    "configs/base.json",
+    "src/__test__/index.spec.ts",
+    "src/index.ts",
+    "src/tsconfig.dev.json",
+    "jest.config.js",
+    "tsconfig.json",
+    "tslint.json",
+    "package.json"
+  ];
+
+  it("basic test", () => {
+    const appName = 'basicTest';
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appName })
+      .then(() => {
+        const assertFiles = allFile.map(file => {
+          return appName + "/" + file;
+        });
+
+        assert.file(assertFiles);
+        assert.fileContent(`${appName}/package.json`, `"name": "${appName}"`);
+      });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("name test", () => {
+    const appName = 'nameTest';
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appName })
+      .then(() => {
+        assert.fileContent(`${appName}/package.json`, `"name": "${appName}"`);
+      });
+  });
+
+  it("no use jest", () => {
+    const appName = 'jestTest';
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appName, useJest: false })
+      .then(() => {
+        assert.noFile(["jest.config.js"]);
+      });
+  });
+
+  it("no use tslint", () => {
+    const appName = 'tslintTest';
+    return helpers
+      .run(path.join(__dirname, "../generators/app"))
+      .withPrompts({ appName, useTslint: false })
+      .then(() => {
+        assert.noFile(["tslint.json"]);
+      });
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,105 +1,119 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const path = require('path');
-const beautify = require('gulp-beautify');
-const merge = require('lodash.merge');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
+const beautify = require("gulp-beautify");
+const merge = require("lodash.merge");
 
-let baseConfig = require('./baseConfig');
+let baseConfig = require("./baseConfig");
 
 module.exports = class extends Generator {
-  constructor(args, opts) {
+  constructor (args, opts) {
     super(args, opts);
     // eslint-disable-next-line
     this.registerTransformStream(beautify({ indent_size: 4 }));
   }
-
-  prompting() {
+  
+  prompting () {
     // Have Yeoman greet the user.
     this.log(
       yosay(
-        `Welcome to the doozie ${chalk.red('generator-modern-typescript')} generator!`
+        `Welcome to the doozie ${chalk.red(
+          "generator-modern-typescript"
+        )} generator!`
       )
     );
-
+    
     const prompts = [
       {
-        type: 'input',
-        name: 'appName',
+        type: "input",
+        name: "appName",
         message: "What's your app name?"
       },
       {
-        type: 'confirm',
-        name: 'useTslint',
-        message: 'Use tslint?',
+        type: "confirm",
+        name: "useTslint",
+        message: "Use tslint?",
         default: true
       },
       {
-        type: 'confirm',
-        name: 'useJest',
-        message: 'Use jest?',
+        type: "confirm",
+        name: "useJest",
+        message: "Use jest?",
         default: true
       }
     ];
-
+    
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
-
-  writing() {
+  
+  writing () {
     const { appName, useJest, useTslint } = this.props;
     const resolvePath = target => {
       return path.join(appName, target);
     };
-
-    this.fs.copy(this.templatePath('configs'), resolvePath('configs'));
-    this.fs.copy(this.templatePath('tsconfig.json'), resolvePath('tsconfig.json'));
-
+    
+    this.fs.copy(this.templatePath("configs"), resolvePath("configs"));
+    this.fs.copy(
+      this.templatePath("tsconfig.json"),
+      resolvePath("tsconfig.json")
+    );
+    
     if (useJest) {
-      this.fs.copy(this.templatePath('src'), resolvePath('src'));
-      this.fs.copy(this.templatePath('jest.config.js'), resolvePath('jest.config.js'));
-    } else {
-      this.fs.copy(this.templatePath('src/index.ts'), resolvePath('src/index.ts'));
+      this.fs.copy(this.templatePath("src"), resolvePath("src"));
       this.fs.copy(
-        this.templatePath('src/tsconfig.dev.ts'),
-        resolvePath('src/tsconfig.dev.ts')
+        this.templatePath("jest.config.js"),
+        resolvePath("jest.config.js")
+      );
+    } else {
+      this.fs.copy(
+        this.templatePath("src/index.ts"),
+        resolvePath("src/index.ts")
+      );
+      this.fs.copy(
+        this.templatePath("src/tsconfig.dev.json"),
+        resolvePath("src/tsconfig.dev.json")
       );
     }
-
-    if (useTslint) {
-      this.fs.copy(this.templatePath('tslint.json'), resolvePath('tslint.json'));
-    }
-
+    
     baseConfig.name = appName;
+    if (useTslint) {
+      this.fs.copy(
+        this.templatePath("tslint.json"),
+        resolvePath("tslint.json")
+      );
+    }
+    
     if (useJest) {
       baseConfig = merge({}, baseConfig, {
         scripts: {
-          test: 'jest'
+          test: "jest"
         },
         devDependencies: {
-          '@types/jest': '^22.2.3',
-          jest: '^22.4.3',
-          'ts-jest': '^22.4.6'
+          "@types/jest": "^22.2.3",
+          jest: "^22.4.3",
+          "ts-jest": "^22.4.6"
         }
       });
     }
-
+    
     if (useTslint) {
       baseConfig = merge({}, baseConfig, {
         scripts: {
-          lint: 'tslint --project ./src/tsconfig.dev.json --fix'
+          lint: "tslint --project ./src/tsconfig.dev.json --fix"
         },
         devDependencies: {
-          tslint: '^5.10.0',
-          'tslint-plugin-prettier': '^1.3.0',
-          prettier: '^1.12.1'
+          tslint: "^5.10.0",
+          "tslint-plugin-prettier": "^1.3.0",
+          prettier: "^1.12.1"
         }
       });
     }
-
-    this.fs.write(resolvePath('package.json'), JSON.stringify(baseConfig));
+    
+    this.fs.write(resolvePath("package.json"), JSON.stringify(baseConfig));
   }
 };
